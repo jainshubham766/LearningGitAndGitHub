2fd67fc4e9d12fd843ba19a5df0f10bd54a6d55d
Shubham Jain <jainshubham766gmail.com>
2020-09-22 16:59:11 +0530

    New 2dev Branch Updated

--- HelloWorld/First.cpp
+++ HelloWorld/First.cpp
@@ -2,8 +2,7 @@
 int main()
 {
 	std::cout << "New sub dev part";
-	std::cout << "IN DEv branch";
-	std::cout << "HELLO WORLD";
-	std::cout << "New sub dev part";
+	std::cout << "IN Dev 2 branch";
+	std::cout << "IN Dev 2 branch";
 	return 0;
 }
